f1890d323955a32432c21a261bfb586489e43413
francis.pereira <francis.pereira@evoluservices.com>
2025-10-24 10:24:35 -0300

    Erro de formatação do CPF/CNPJ após concluir transação no Pinpad
    Task: https://evoluservices.atlassian.net/browse/AE-70

--- src/utils/formatDocument.tsx
+++ src/utils/formatDocument.tsx
@@ -1,7 +1,8 @@
-export function formatDocument(value: string): string {
+export function formatDocument(value: any): string {
     if (!value) return "";
 
-    const onlyNumbers = value.replace(/\D/g, "");
+    const str = String(value);
+    const onlyNumbers = str.replace(/\D/g, "");
 
     if (onlyNumbers.length === 11) {
         // CPF: 000.000.000-00
@@ -13,6 +14,5 @@ export function formatDocument(value: string): string {
         return onlyNumbers.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
     }
 
-    // fallback (não reconhecido)
-    return value;
+    return str;
 }
